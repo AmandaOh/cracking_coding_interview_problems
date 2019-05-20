7a6edf14fecf1a90c6b6ac43872dee635b948013
AmandaOh <amandaoh@thoughtworks.com>
2019-05-20 14:15:30 +0800

    Problem 2.6 Page 95- Palindrome (Refactored)

--- chapter_2_linked_lists/Palindrome.java
+++ chapter_2_linked_lists/Palindrome.java
@@ -1,5 +1,9 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class Palindrome {
@@ -7,50 +11,54 @@ class Palindrome {
     //assuming singly linked list
     private boolean solve(LinkedListNode<Character> list) {
         LinkedListNode<Character> head = list;
-        LinkedListNode<Character> tail = list;
-        int length = 1;
+        LinkedListNode<Character> runner = list;
+        Queue<Character> startOfListToMid = Collections.asLifoQueue(new LinkedList<>());
 
-        //navigate to tail of list
-        while(tail.next != null) {
-            tail = tail.next;
-            length++;
+        while(runner != null && runner.next != null) {
+            startOfListToMid.add(head.data);
+            head = head.next;
+            //move runner twice as fast as head
+            runner = runner.next.next;
         }
 
-
-        //place pointer after mid point of list
-        LinkedListNode<Character> nodeAfterMid = head;
-        int midIndex = 0;
-
-        if (length % 2 == 0) {
-            midIndex = (length / 2);
-            for (int i = 0; i < midIndex + 1; i++) {
-                nodeAfterMid = nodeAfterMid.next;
-            }
-        } else {
-            midIndex = (length - 1) / 2;
-            for (int i = 0; i < midIndex + 1; i++) {
-                nodeAfterMid = nodeAfterMid.next;
-            }
-        }
-
-        //get start to midpoint
-        for (int i = 0; i < midIndex - 1; i++) {
+        //skip middle character if odd length
+        if (runner != null) {
             head = head.next;
         }
-        head.next = null;
 
-        //reverse arrangement of nodes after midpoint
-        LinkedListNode<Character> pointer = nodeAfterMid;
-
-        while(pointer != tail) {
-            LinkedListNode<Character> node = new LinkedListNode<>();
-            node.data = pointer.data;
-            node.next = tail.next;
-            tail.next = node;
-            pointer = pointer.next;
+        //compare list after midpoint to start of list to determine if palindrome
+        while(head != null) {
+            if (head.data != startOfListToMid.poll()) {
+                return false;
+            }
+            head = head.next;
         }
 
-        return list.equals(tail);
+        return true;
+    }
+
+    @Test
+    void redder_returns_true() {
+        LinkedListNode<Character> node1 = new LinkedListNode<>();
+        node1.data = 'r';
+        LinkedListNode<Character> node2 = new LinkedListNode<>();
+        node2.data = 'e';
+        LinkedListNode<Character> node3 = new LinkedListNode<>();
+        node3.data = 'd';
+        LinkedListNode<Character> node4 = new LinkedListNode<>();
+        node4.data = 'd';
+        LinkedListNode<Character> node5 = new LinkedListNode<>();
+        node5.data = 'e';
+        LinkedListNode<Character> node6 = new LinkedListNode<>();
+        node6.data = 'r';
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+
+        assertTrue(solve(node1));
     }
 
     @Test
